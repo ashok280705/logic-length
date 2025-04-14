cf5d0f54aa3b2aae4c9dcbb4246719c8b02465df
Ashok mayekar <anuj.mayekar001@gmail.com>
2025-04-14 13:25:28 +0530

    Add root level server starter

---
+++ server.js
@@ -0,0 +1,5 @@
+// Root level server starter to help with module resolution
+import './src/server/index.js';
+
+// This file exists just to help with proper module resolution
+// when running on Render 
